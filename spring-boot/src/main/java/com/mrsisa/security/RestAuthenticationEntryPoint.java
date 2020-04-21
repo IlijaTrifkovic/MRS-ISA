92bc714879099dcb94071bad17608f296e24ae21
IlijaTrifkovic <trifkovic69@gmail.com>
2020-04-21 17:53:38 +0200

    \#2.3 prijava korisnika [backend]

--- spring-boot/src/main/java/com/mrsisa/security/RestAuthenticationEntryPoint.java
+++ spring-boot/src/main/java/com/mrsisa/security/RestAuthenticationEntryPoint.java
@@ -1,4 +1,4 @@
-package com.mrsisa.service.auth;
+package com.mrsisa.security;
 
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
